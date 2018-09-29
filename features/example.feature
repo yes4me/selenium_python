3a8734f1c25652af8c6b941bb0319bc94376be56
Thomas Nguyen <yes4me@hotmail.com>
2018-09-29 08:26:37 -0700

    complete behave and selenium

--- features/example.feature
+++ features/example.feature
@@ -4,3 +4,8 @@ Feature: Showing off behave
     Given we have behave installed
     When we implement 5 tests
     Then behave will test them for us!
+
+  Scenario: Run a simple test pm google.com
+    Given user goes to http://www.google.com
+    When user searches "123"
+    Then user closes browser
---
+++ features/steps/selenium_steps.py
@@ -0,0 +1,20 @@
+from behave import given, when, then, step
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
+
+@given(u'user goes to {website}')
+def step_impl(context, website):
+	driver.get(website)
+
+@when(u'user searches "{text_to_search}"')
+def step_impl(context, text_to_search):
+    search_box = driver.find_element_by_name('q')
+    search_box.send_keys(text_to_search)
+    search_box.submit()
+
+@then(u'user closes browser')
+def step_impl(context):
+    time.sleep(3)
+    driver.quit()
