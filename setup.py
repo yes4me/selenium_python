b702a35ef823a8186b445b02df4e3c67dde6fc6c
Thomas Nguyen <yes4me@hotmail.com>
2018-10-12 08:40:07 -0700

    update README.MD

--- setup.py
+++ setup.py
@@ -1,15 +1,25 @@
+import os
 import setuptools
 
 
-with open("README.MD", "r") as fh:
-    long_description = fh.read()
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setuptools.setup(
     name='python_selenium_framework',
     version='1.0.0',
     author='Thomas Nguyen',
     author_email="thomas.ejob@gmail.com",
+    url='https://github.com/yes4me',
+    license='GNU GPLv3',
     description='Selenium framework for Python',
-    long_description=long_description,
-    packages=setuptools.find_packages(),
+    long_description=read('README.MD'),
+    platforms=['Raspberry Pi 3B+'],
+    packages=setuptools.find_packages(exclude=['docs', 'tests*']),
+    install_requires=[
+        'behave',
+        'selenium'
+    ],
 )
